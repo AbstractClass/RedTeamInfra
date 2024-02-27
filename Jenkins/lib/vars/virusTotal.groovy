d550ef14ebe1d5ac48f004a19609bf26d11cd90f
Cloud <connor.macleod96@hotmail.ca>
2024-02-27 16:32:32 -0500

    small fix

--- Jenkins/lib/vars/virusTotal.groovy
+++ Jenkins/lib/vars/virusTotal.groovy
@@ -3,7 +3,7 @@ def call(String binaryLocation) {
     withCredentials(bindings: [string(credentialsId: 'VirusTotal', variable: 'VTCLI_APIKEY')]) {
         withEnv(["binaryLocation=${binaryLocation}"]) {
             powershell script: '''
-                $result = $(vt scan file $env:binaryLocation --silent).Split()[1]
+                $result = vt scan file $env:binaryLocation --silent
                 echo $result
                 $hash = $result.Split()[1]
                 $report = vt analysis $hash
